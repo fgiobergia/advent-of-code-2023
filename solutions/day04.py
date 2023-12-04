9935c947d8b1100f8f3fc00741166a0c87b98700
fgiobergia <flavio.giobergia@gmail.com>
2023-12-04 11:44:05 +0100

    day 4️⃣

---
+++ solutions/day04.py
@@ -0,0 +1,14 @@
+with open("day04.input") as f:
+    cards = [ line.split(":")[1].strip().split("|") for line in f ]
+cards = [ ({x for x in win.strip().split(" ") if x}, {x for x in played.strip().split(" ") if x}) for win, played in cards ]
+
+print(sum( (1 << len(w & p)) // 2 for w, p in cards ))
+
+cards = [ [c, 1] for c in cards ]
+
+for i, ((w,p), cnt) in enumerate(cards):
+    o = len(w & p)
+    for j in range(i + 1, i + 1 + o):
+        cards[j][1] += cnt
+
+print(sum(c[1] for c in cards))
